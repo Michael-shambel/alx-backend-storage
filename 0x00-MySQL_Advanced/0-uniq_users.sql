da30d2272efe9a2a337f74f8da265bba743c5d52
michael-shambel <Michaelshambel10@gmail.com>
2024-07-11 13:01:18 +0300

    task 0 up1

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,5 @@
 -- script that creates a table user that contain differnt column
 -- the database name is called holberton
-CREATE DATABASE IF NOT EXISTS holberton;
-
-USE holberton;
 
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
