97b090a1aeef2b93cefebbfb9edcbb176ba4978e
michael-shambel <Michaelshambel10@gmail.com>
2024-07-11 23:32:12 +0300

    tasks 8

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- script that creates a view need_meeting that list all students that hav a score
+-- under 80 (strict) and no last_meeting and more than one month
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT
+)
+BEGIN
+    DECLARE avg_score FLOAT;
+    SELECT AVG(score) INTO avg_score
+    FROM corrections
+    WHERE user_id = user_id;
+    UPDATE users
+    SET average_score = avg_score
+    WHERE id = user_id;
+END $$
+DELIMITER ;
