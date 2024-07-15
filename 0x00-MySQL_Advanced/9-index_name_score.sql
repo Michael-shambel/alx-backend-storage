9df6d0f80fc48deadc4e05283dec763f8f8e63a8
michael-shambel <Michaelshambel10@gmail.com>
2024-07-15 16:26:15 +0300

    task 9up2

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,4 @@
 -- script that create a mysql index on the table
 -- and the firstletter of name and the score
+DROP INDEX IF EXISTS idx_name_first_score ON names;
 CREATE INDEX idx_name_first_score ON names(LEFT(name, 1), score);
