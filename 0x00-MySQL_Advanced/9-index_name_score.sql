bb7dd374c58ab6f1a1c6a7a4b0876b76787e1349
michael-shambel <Michaelshambel10@gmail.com>
2024-07-15 16:25:20 +0300

    task 9up1

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- script that create a mysql index on the table
 -- and the firstletter of name and the score
-CREATE INDEX idx_name_first_score ON names(LEFT(name(1)), score);
+CREATE INDEX idx_name_first_score ON names(LEFT(name, 1), score);
