0e46a3e2ec951cadebbed0005aa488b2f1ac223a
michael-shambel <Michaelshambel10@gmail.com>
2024-07-16 13:29:46 +0300

    task 9 up

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -10,5 +10,5 @@ def insert_school(mongo_collection, **kwargs):
     collection and data provided and the id or the 
     new object will be return
     """
-    result = mongo_collection.insert(kwargs)
+    result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
