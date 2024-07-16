b7a854497c9abdfc75538288bb8ddaf36f4867b9
michael-shambel <Michaelshambel10@gmail.com>
2024-07-17 01:25:50 +0300

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python3
 """
-python script that provides some stats about nginx logs
-stored in mongoDB 
+python script that provides some stats about Nginx logs stored in mongoDb
 """
+from pymongo import MongoClient
+
+
+def logs(mongo_collection):
+    """
+    this function accept mongo collection and return collections
+    """
+    total_logs = mongo_collection.count_documents({})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    methods_count = {mongo_collection.count_documets({"method": method}) for method in methods}
+    status_check = mongo_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for metd in methods:
+        print(f"method {metd}: {methods_count[metd]}")
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    client = MongoClient()
+    nginx_collection = client.logs.nginx
+    logs(nginx_collection)
