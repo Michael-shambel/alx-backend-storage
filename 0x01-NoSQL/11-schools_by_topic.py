2d84466259f1f23d8df3fef640a81635a57e623d
michael-shambel <Michaelshambel10@gmail.com>
2024-07-16 14:40:50 +0300

    task11

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+function that return 
+the list of school having the specifc topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    the function that accept mongo collection and topic and return
+    the name school that have the specific topics
+    """
+    schoolList = mongo_collection.find({"topics": topic})
+    return list(schoolList)
