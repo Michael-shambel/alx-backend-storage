4cae85f2b4422d0eb86b0fd7d5e049c6e3314d73
michael-shambel <Michaelshambel10@gmail.com>
2024-07-17 21:29:04 +0300

    task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+creating a cache class with the instance of radis and flushdb
+creating store method thatakes data argumetn and return string
+and generate a random key
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    cache class that intialize redis and flushdb
+    """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]):
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
