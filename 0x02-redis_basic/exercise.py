1fe84ad0ea253014447c1ac60e36991b390e53fe
michael-shambel <Michaelshambel10@gmail.com>
2024-07-17 21:31:07 +0300

    task 0up1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,7 +17,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]):
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
